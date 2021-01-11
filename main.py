c95cf94687554a4db448540d7bc2fae0cec42e3f
Chris <chrisedburt@gmail.com>
2021-01-11 20:32:28 +0900

    Add new folders. Add images for guide and door and experienced player. Step-three methods.

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ Main bot file.
 
 """
 
+import os
+import time
 import sys
+
 import startup
 import vision
 import misc
@@ -11,10 +14,6 @@ import behaviour
 import input
 import skills
 import logging as log
-
-import os
-import time
-from PIL import Image
 
 import step_one
 import step_two
@@ -30,27 +29,14 @@ import step_eleven
 
 # Test ----------------------------------------------------------------------------------------------------------------------
 
-# login using credentials, type full username followed by the password.
-# behaviour.login_full()
-
+# login using credentials, type full username followed by the password. Dont wait for after login screen to appear.
+#behaviour.login_full()
 behaviour.login_basic()
 
-# time.sleep(15)
-
 logged_in = vision.Vision(ltwh=vision.display,
-                                   image='./images/minimap/orient.png',
-                                   loop_num=50, loop_sleep_range=(1000, 2000)).wait_for_image()
+image='./images/minimap/orient.png',
+loop_num=50, loop_sleep_range=(1000, 2000)).wait_for_image()
 
-# log.info('Open inventory screen if closed, otherwise nothing.')
-# behaviour.open_side_stone('inventory')
-
-gielnor_guide = [os.path.join('./images/game-screen/tutorial-island/gielnor-guide/',f) for f in os.listdir('./images/game-screen/tutorial-island/gielnor-guide/') if f.endswith('.png')]
-print(gielnor_guide)
-
-for i in gielnor_guide:
-    log.info('gielbor_guide ' + i)
-    gielnor_guide1 = vision.Vision(ltwh=vision.client,
-    image = i,    
-    loop_num=1).click_image()
+step_three.third_step()
 
 # Test ----------------------------------------------------------------------------------------------------------------------
--- step_three.py
+++ step_three.py
@@ -3,4 +3,51 @@ Step three file.
 
 """
 
+import os
+import vision
+import logging as log
+import pyautogui as pag
+
 # Step Three Gielnor Guide -------------------------------------------------------------------------------------------------
+
+def third_step():
+    speak_to_gielnor_guide()
+    click_experienced_player()
+    click_spanner_icon()
+    speak_to_gielnor_guide()
+    click_door()
+
+def speak_to_gielnor_guide():
+    # locate and click experienced player response
+    gielnor_guide = [os.path.join('./images/game-screen/tutorial-island/step-three/gielnor-guide/',f) for f in os.listdir('./images/game-screen/tutorial-island/step-three/gielnor-guide/') if f.endswith('.png')]
+    #print(gielnor_guide)
+
+    for i in gielnor_guide:
+        log.info('gielbor_guide ' + i)
+        gielnor_guide1 = vision.Vision(ltwh=vision.client,
+        image = i,    
+        loop_num=1).click_image()
+
+    pag.press('space', presses=5)
+
+def click_experienced_player():
+    # locate and click experienced player response
+    experienced_player = vision.Vision(ltwh=vision.client,
+    image='./images/game-screen/tutorial-island/step-three/dialoge_responses/experienced.png',
+    loop_num=1).click_image()
+
+def click_spanner_icon():
+    # locate and click closed spanner(settings) icon
+    experienced_player = vision.Vision(ltwh=vision.client,
+    image='./images/side-stones/closed/settings.png',
+    loop_num=1).click_image()
+
+def click_door():
+    # locate and click door
+    door = [os.path.join('./images/game-screen/tutorial-island/step-three/',f) for f in os.listdir('./images/game-screen/tutorial-island/step-three/') if f.endswith('.png')]
+    #print(door)
+    for i in door:
+        log.info('door ' + i)
+        door = vision.Vision(ltwh=vision.client,
+        image = i,    
+        loop_num=1).click_image()
