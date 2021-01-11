2230abe65f8637e3475314d68e6ece03f66e04c3
nzteep <74941648+nzteep@users.noreply.github.com>
2021-01-11 22:23:38 +1300

    add generate_username function

--- step_two.py
+++ step_two.py
@@ -1,6 +1,29 @@
 """
 Step two file.
 
+Generating a username from dinopass
+
+an_name is alpha numeric name
+name is only alpha
+
+
 """
 
 # Step Two Choose Account Name -------------------------------------------------------------------------------------------------
+
+import requests
+
+
+def generate_username():
+    uri = 'https://www.dinopass.com/password/simple'
+    r = requests.get(uri)
+    an_name = r.text
+    name = ''.join(filter(str.isalpha, an_name))
+
+    return an_name, name
+
+
+# For testing
+#t1, t2 = generate_username()
+#print(t1)
+#print(t2)
